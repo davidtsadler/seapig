cceadf394e7d4f47d9d1faf2e0ee74e421ff281f
David T. Sadler <davidtsadler@googlemail.com>
2013-01-26 18:16:12 +0000

    Add default grunt task.
    Default grunt task will simply run lint task on the grunt.js file.

---
+++ grunt.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        lint: {
+            all: ['grunt.js']
+        },
+
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                node: true,
+                es5: true,
+                strict: false 
+            }
+        }
+    });
+
+    // Default task.
+    grunt.registerTask('default', 'lint');
+};
