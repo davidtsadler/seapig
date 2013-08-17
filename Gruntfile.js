00fba47c1b6d65c02e9e1c9242b82b87d9e650f7
David T. Sadler <davidtsadler@googlemail.com>
2013-08-17 14:36:25 +0100

    Update dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,6 @@ module.exports = function(grunt) {
                 boss: true,
                 eqnull: true,
                 node: true,
-                es5: true,
                 strict: false 
             }
         }
