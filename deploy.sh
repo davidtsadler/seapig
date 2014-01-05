662857857cf2da87bf4fe9a20bb201cbab1024f0
David T. Sadler <davidtsadler@googlemail.com>
2014-01-05 18:17:37 +0000

    GZip files before deploying them.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-# CSS (Cache: expire in 1 week)
-s3cmd sync --acl-public --exclude '*.*' --include '*.css' --add-header="Cache-Control: max-age=604800"  dist/css/ s3://seapig.net/css/
+# GZIP files.
+find dist -type f \( -iname '*.xml' -or -iname '*.txt' -or -iname '*.css' -or -iname '*.js' -or -iname '*.html' \) -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
+
+# CSS (Cache: 1 week)
+s3cmd sync --acl-public --exclude '*.*' --include '*.css' -m "text/css" --add-header="Cache-Control: max-age=604800" --add-header="Content-Encoding: gzip" --add-header="Vary: Accept-Encoding" dist/css/ s3://seapig.net/css/
 
 # HTML (Cache: 1 week)
-s3cmd sync --acl-public --exclude '*.*' --include  '*.html' --add-header="Cache-Control: max-age=604800, must-revalidate" dist/ s3://seapig.net/
+s3cmd sync --acl-public --exclude '*.*' --include  '*.html' -m "text/html" --add-header="Cache-Control: max-age=604800, must-revalidate" --add-header="Content-Encoding: gzip" --add-header="Vary: Accept-Encoding" dist/ s3://seapig.net/
 
 # Sitemap.xml (Cache: 1 week)
-s3cmd sync --acl-public --exclude '*.*' --include  'sitemap.xml' --add-header="Cache-Control: max-age=604800, must-revalidate" dist/ s3://seapig.net/
+s3cmd sync --acl-public --exclude '*.*' --include  'sitemap.xml' -m "application/xml" --add-header="Cache-Control: max-age=604800, must-revalidate" --add-header="Content-Encoding: gzip" --add-header="Vary: Accept-Encoding" dist/ s3://seapig.net/
 
-# Robots.txt (Cache: expire in 10weeks)
-s3cmd sync --acl-public --exclude '*.*' --include 'robots.txt' --add-header="Expires: Sat, 21 Nov 2020 00:00:00 GMT" --add-header="Cache-Control: max-age=6048000"  dist/ s3://seapig.net/
+# Robots.txt (Cache: 10 weeks)
+s3cmd sync --acl-public --exclude '*.*' --include 'robots.txt' -m "text/plain" --add-header="Cache-Control: max-age=6048000" --add-header="Content-Encoding: gzip" --add-header="Vary: Accept-Encoding" dist/ s3://seapig.net/
 
 # Sync: remaining files & delete removed
 s3cmd sync --acl-public --delete-removed  dist/ s3://seapig.net/
